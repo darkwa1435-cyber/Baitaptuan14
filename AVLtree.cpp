50a913e791159e38551a7a20908669d3084ea8e0
TranNguyenTanDung <darkwa1435@gmail.com>
2026-06-11 21:38:57 +0700

    test xoay trai

--- AVLtree.cpp
+++ AVLtree.cpp
@@ -26,6 +26,14 @@ void rotateright(node* & k2){
     k1 -> height = max(getheight(k1 -> left), getheight(k2 -> right)) + 1;
     k2 = k1;
 }
+void rotateleft(node * &k1){
+    node* k2 = k1 -> right;
+    k1 -> right = k2 -> left;
+    k2 -> left = k1;
+    k1 -> height = max(getheight(k1 -> left),getheight(k1 -> right)) + 1;
+    k2 -> height = max(getheight(k2->left), getheight(k2->right)) + 1;
+    k1 = k2;
+}
 void preorder(node* root){
     if (root != NULL){
         cout << root -> data << " ";
@@ -35,14 +43,14 @@ void preorder(node* root){
 }
 int main(){
    /* int a[] = {32, 51, 27, 83, 96, 11, 45, 75, 66}; */
-   node* root = new node(30, NULL, NULL, 3);
-   root -> left = new node(20, NULL, NULL, 2);
-   root -> left -> left = new node(10, NULL, NULL, 1);
+   node* root = new node(10, NULL, NULL, 0);
+   root -> right = new node(20, NULL, NULL, 1);
+   root -> right -> right = new node(30, NULL, NULL, 2);
    cout << "cay ban dau: ";
     preorder(root);
     cout << endl;
-    rotateright(root);
-    cout << "cay sau khi xoay phai: ";
+    rotateleft(root);
+    cout << "cay sau khi xoay trai: ";
     preorder(root);
     cout << endl;
 }
